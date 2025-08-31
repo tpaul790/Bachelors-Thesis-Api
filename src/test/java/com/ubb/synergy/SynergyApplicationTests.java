373fbb9d56b1f6d70d02e4ab27ceddc3ebf9fea9
Titieni Paul <154063842+tpaul790@users.noreply.github.com>
2025-08-31 11:14:05 +0300

    made the minimal backend setup

---
+++ src/test/java/com/ubb/synergy/SynergyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ubb.synergy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SynergyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
